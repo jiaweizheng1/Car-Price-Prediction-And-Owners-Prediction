c388a47f5da42f98f0b05cb0b91e223285887384
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2023-05-23 21:59:32 -0700

    Font End Adjustment For Two Types Of Models(Price Predict + Classification)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
 import webbrowser as wb
@@ -6,7 +6,7 @@ from threading import Timer
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl','rb'))
+model1 = pickle.load(open('model.pkl','rb'))
 
 model = ''
 
@@ -16,37 +16,66 @@ def init():
 
 @app.route('/select_model', methods=['POST','GET'])
 def select_model():
+    global model 
     model = request.form.get('model', '')
 
     # DEBUG
     print(model)
     # DEBUG
 
-    return render_template('car_predict.html', model=model)
+    return render_template('car_predict.html', model = model)
 
-@app.route('/predict', methods=['POST','GET'])
-def predict():
+@app.route('/predict1', methods=['POST','GET'])
+def predict1():
+    global model
+
     try:
         int_features=[int(x) for x in request.form.values()]
     except:
-        return render_template('car_predict.html', pred = 'Invalid Input.')
+        return render_template('car_predict.html', pred = 'Invalid Input.', model = model)
 
     final=[np.array(int_features)]
     
     # DEBUG
+    print(model)
     print(int_features)
     print(final)
     # DEBUG
 
-    prediction=model.predict_proba(final)
+    prediction=model1.predict_proba(final)
     output = '{0:.{1}f}'.format(prediction[0][1], 2)
 
     if output > str(0.5):
-        return render_template('car_predict.html', pred = 'You are getting scammed. Predicted price is {}.'.format(output))
+        return render_template('car_predict.html', pred = 'You are getting scammed. Predicted price is {}.'.format(output), model = model)
     else:
-        return render_template('car_predict.html', pred = 'Your are not getting scammed. Predicted is {}.'.format(output))
+        return render_template('car_predict.html', pred = 'Your are not getting scammed. Predicted is {}.'.format(output), model = model)
+    
+@app.route('/predict2', methods=['POST','GET'])
+def predict2():
+    global model
+
+    try:
+        int_features=[int(x) for x in request.form.values()]
+    except:
+        return render_template('car_predict.html', pred = 'Invalid Input.', model = model)
+
+    final=[np.array(int_features)]
+    
+    # DEBUG
+    print(model)
+    print(int_features)
+    print(final)
+    # DEBUG
+
+    prediction=model1.predict_proba(final)
+    output = '{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output > str(0.5):
+        return render_template('car_predict.html', pred = 'You are getting scammed. Predicted price is {}.'.format(output), model = model)
+    else:
+        return render_template('car_predict.html', pred = 'Your are not getting scammed. Predicted is {}.'.format(output), model = model)
 
 if __name__ == '__main__':
-    # Timer(1, wb.open_new("http:localhost:5000/")).start()
-    # app.run(debug=False)
-    app.run(debug=True)
+    Timer(1, wb.open_new("http:localhost:5000/")).start()
+    app.run(debug=False)
+    # app.run(debug=True)
