f89a836e687c1459ad2bd126a0b00f7f0a03efdd
jiaweizheng1 <jiaweizheng416528@gmail.com>
2023-05-28 19:14:14 -0700

    Add Regression_LineReg and Regression_RF to flask font end

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from threading import Timer
 
 app = Flask(__name__)
 
+model1 = pickle.load(open('models/Regression_LinReg.pkl','rb'))
+
+model2 = pickle.load(open('models/Regression_RF.pkl','rb'))
+
 model3 = pickle.load(open('models/Classification_knn.pkl','rb'))
 
 model4 = pickle.load(open('models/Classification_MLP.pkl','rb'))
@@ -29,7 +33,33 @@ def predict1():
     global model
     print(model)
 
+    age = request.form.get("Age")
+    owners = request.form.get("Number of Previous Owners")
+    kms = request.form.get("Kms Driven")
+    fuel = request.form.get("Fuel Type")
+    seller_type = request.form.get("Seller Type")
+    transmission = request.form.get("Transmission Type")
+
+    age = (int(age))
+    kms = np.log10(int(kms))
+    if(int(owners) <= 1):
+        owners = 0
+    else:
+        owners = 1
+    fuel_Diesel = 1 if fuel.lower() == "diesel" else 0
+    fuel_Petrol = 1 if fuel.lower() == "petrol" else 0
+    seller_type_Dealer = 1 if seller_type.lower() == "dealer" else 0
+    seller_type_Individual = 1 if seller_type.lower() == "individual" else 0
+    transmission_Manual = 1 if transmission.lower() == "manual" else 0
+
+    input_to_model = np.array([[age, kms, owners, fuel_Diesel, fuel_Petrol, seller_type_Dealer, seller_type_Individual, transmission_Manual]])
+
+    if model == "1":
+        prediction_logged = model1.predict(input_to_model)
+    if model == "2":
+        prediction_logged = model2.predict(input_to_model)
     
+    return render_template('car_predict.html', pred = 'Predicted Price of Car is ${}.'.format(int(pow(10, prediction_logged))), model = model)
     
 @app.route('/predict2', methods=['POST','GET'])
 def predict2():
@@ -51,11 +81,10 @@ def predict2():
     fuel_Petrol = 1 if fuel.lower() == "petrol" else 0
     seller_type_Dealer = 1 if seller_type.lower() == "dealer" else 0
     seller_type_Individual = 1 if seller_type.lower() == "individual" else 0
-    seller_type_Trustmark_Dealer = 1 if seller_type.lower() == "trustmark dealer" else 0
     transmission_Automatic = 1 if transmission.lower() == "automatic" else 0
     transmission_Manual = 1 if transmission.lower() == "manual" else 0
 
-    input_to_model = np.array([[age, sell_price, kms, fuel_Diesel, fuel_Other, fuel_Petrol, seller_type_Dealer, seller_type_Individual, seller_type_Trustmark_Dealer, transmission_Automatic, transmission_Manual]])
+    input_to_model = np.array([[age, sell_price, kms, fuel_Diesel, fuel_Other, fuel_Petrol, seller_type_Dealer, seller_type_Individual, transmission_Automatic, transmission_Manual]])
 
     if model == "3":
         prediction_probs = model3.predict_proba(input_to_model)
